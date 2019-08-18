ea3965cb13be4d20d2b1fff48a8dbfd31c085dfa
John Reynolds <jreynoldsvc@gmail.com>
2019-08-18 16:57:53 -0400

    add general_listener

---
+++ src/c++/examples/general_listener.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <memory>
+#include "config.hpp"
+#include "wsapi.hpp"
+
+
+class MsgRecv : public wsapi_cb {
+public:
+  void recv(const std::string &msg) {
+    std::cout << msg << std::endl;
+  }
+};
+
+int main(int argc, char **argv) {
+  if (argc <  3) {
+    std::cerr << "usage : <jsonFile> <msgFile>" << std::endl;
+    std::cerr << "genearal_listener ../server_config.json gdax" << std::endl;
+    return 1;
+  }
+  int cnt = 1;
+  const char *jsonFile = argv[cnt++];
+  const char *name = argv[cnt++];
+  Config c = ConfigReader::instance().get(jsonFile, name);
+  MsgRecv recv;
+  wsapi ws(c.host.c_str(), c.port.c_str(), c.subs, &recv);
+  ws.start();
+  return 0;
+}
+
