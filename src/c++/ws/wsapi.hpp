8aac7eb9f20d2c3bc0411f7dbd050204ceaf6221
jrrpanix <jreynoldsvc@gmail.com>
2021-10-18 19:31:54 -0400

    clean

--- src/c++/ws/wsapi.hpp
+++ src/c++/ws/wsapi.hpp
@@ -2,39 +2,46 @@
 #include <string>
 #include <vector>
 //
-// John Reynolds, Cornelia Consulting LLC, 08/2019
-//
-// Distributed under MIT License
 
-class wsapi_cb {
+////////////////////////////////////////////
+// call back class to recv events from websocket
+////////////////////////////////////////////
+class wsapi_cb
+{
 public:
   virtual void recv(const std::string &) = 0;
 };
 
-class wsapi {
+////////////////////////////////////////////
+// websocket api, (wrapper over beast)
+////////////////////////////////////////////
+class wsapi
+{
 public:
   /*
     host, port, subscription, simple callback class
   */
   wsapi();
-  wsapi(const char *host, const char *port, const char *sub, wsapi_cb *cb, const char *certfile="");
-  wsapi(const char *host, const char *port, const std::vector<std::string> &subs, wsapi_cb *cb, const char *certfile="");
+  wsapi(const char *host,
+	const char *port,
+	const char *sub,
+	wsapi_cb   *cb,
+	const char *certfile="");
 
-  /*
-    example default used on linux in python3 /usr/lib/ssl/certs/653b494a.0
-    this comment on beast is on the cert in root_certificates.hpp not for production
-    https://github.com/boostorg/beast/issues/1702
-    which is why you should set this file
-  */
+  wsapi(const char *host,
+	const char *port,
+	const std::vector<std::string> &subs,
+	wsapi_cb *cb,
+	const char *certfile="");
+
   void set_cert_file(const std::string &certfile); // ssl certificate file
   void set_host(const std::string &host);
   void set_port(const std::string &port);
   void set_subs(const std::vector<std::string> &subs);
   void set_cb(wsapi_cb *cb);
   
-  /*
-    start the websocket
-  */
+
+  // start websocket
   void start();
 
 private:
